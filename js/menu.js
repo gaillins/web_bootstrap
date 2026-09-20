1b23170e168bc4a4372c3274070c5163ab74a2ef
gaillins <ikaewalin@gmail.com>
2026-09-20 10:09:50 +0700

    update footer v1

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 // โหลด menu.html
 fetch("menu.html")
-  .then(res => res.text())
-  .then(data => {
+  .then((res) => res.text())
+  .then((data) => {
     document.getElementById("menu-container").innerHTML = data;
+  });
+fetch("footers.html")
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
   });
